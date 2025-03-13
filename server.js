48cc2c99cc79c1f6f2438f4534a52b9db72f1395
MarcoApunto <ferreiras.marcoa@gmail.com>
2025-03-13 05:29:38 +0100

    Added static page config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express, { json, urlencoded } from "express"
-import path from "path"
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from "dotenv"
 import cors from "cors"
 
@@ -17,6 +18,9 @@ import dashboardRoutes from "./routes/dashboard.js"
 import cartItemRoutes from "./routes/cartItem.js"
 
 dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -42,8 +46,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-const PORT = process.env.PORT
 
 sync.syncDDBB().then(() => {
 	app.listen(PORT, () => {
